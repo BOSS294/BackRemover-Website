c65a6972d4f4f89ec34eaf0bc4a05dfed3594f29
Mayank Chawdhari <72921622+BOSS294@users.noreply.github.com>
2024-08-12 09:25:23 +0530

    FLOATING NAV BAR DONE
    MORE ENHANCEMENTS NEEDED

---
+++ Assets/Main/Resources/nav-bar.php
@@ -0,0 +1,14 @@
+<div class="nav">
+    <a href="#home" class="nav-item">
+        <span class="material-icons">home</span>
+    </a>
+    <a href="#about" class="nav-item">
+        <span class="material-icons">info</span>
+    </a>
+    <a href="#services" class="nav-item">
+        <span class="material-icons">build</span>
+    </a>
+    <a href="#contact" class="nav-item">
+        <span class="material-icons">contact_mail</span>
+    </a>
+</div>
